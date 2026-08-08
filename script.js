aabbb66bd35e6138475ca9a21600dbbc528f216e
raheemsoomro890 <raheemsoomro282@gmail.com>
2026-08-08 21:29:58 +0500

    Fix syntax errors and improve event listener setup

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-const container -
-
+const container =
 document.getElementById('container');
-
 const registerBtn =
-
 document.getElementById('register');
-
 const loginBtn =
-
 document.getElementById('login');
-
 registerBtn.addEventListener('click', ()
 
-container.classList.add("active");
+=> {
+   container.classList.add("active");
 
 });
-
 LoginBtn.addEventListener('click', () =>
-
-container.classList.remove("active");
+  {
+     container.classList.remove("active");
 
 });
