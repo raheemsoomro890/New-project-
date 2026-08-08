b687bc5e6e55fd8c57242eda59e63fc65cc0c26a
raheemsoomro890 <raheemsoomro282@gmail.com>
2026-08-08 19:21:34 +0500

    Add event listeners for register and login buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+const container -
+
+document.getElementById('container');
+
+const registerBtn =
+
+document.getElementById('register');
+
+const loginBtn =
+
+document.getElementById('login');
+
+registerBtn.addEventListener('click', ()
+
+container.classList.add("active");
+
+});
+
+LoginBtn.addEventListener('click', () =>
+
+container.classList.remove("active");
+
+});
